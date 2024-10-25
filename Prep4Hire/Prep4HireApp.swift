55b3cba27e949cbc10f718cf0818cba91cf6e26f
Liellison Menezes <liellisonjoseph@gmail.com>
2024-10-25 05:53:15 -0300

    Initial Commit

---
+++ Prep4Hire/Prep4HireApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prep4HireApp.swift
+//  Prep4Hire
+//
+//  Created by Liellison Menezes on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Prep4HireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
